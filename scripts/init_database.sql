bee2e624c1cf3fdad1033af66d0371d7b3feaf34
mh-uday <87147065+mh-uday@users.noreply.github.com>
2025-10-05 14:10:20 +0200

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,21 @@
+/*
+=================================================
+Create Database and Schemas
+=================================================
+*/
+
+
+-- Create Databasae 'DataWarehouse'
+
+CREATE DATABASE DataWarehouse;
+
+-- Create Schemas
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
